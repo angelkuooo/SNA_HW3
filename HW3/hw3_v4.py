ead037451b5cffa6d1159680ae1f031b6d904aa1
wlilian383 <wlilian383@gmail.com>
2018-06-03 03:20:29 +0800

    add hw3_v4.py
    加入avg path討論
    degree排序list中，兩點距離要大於某數，才會加入ans_list
    參數設為: 1, 4, 8.66, 5
    degree最低: 39, 15, 3, 10
    前60個 degree list: 64,61,54,53,53,53,52,52,51,51,...35

--- HW3/hw3_v4.py
+++ HW3/hw3_v4.py
@@ -33,6 +33,7 @@ if __name__ == '__main__':
 	raw_data = readfile('networkdata.txt')
 	node_list = (set([item for sublist in raw_data for item in sublist]))
 	ans_list = []
+	#avg_path_list = []
 	G = nx.Graph()
 	G.add_nodes_from(node_list)
 	for edges_num in range(len(raw_data)):
@@ -40,8 +41,25 @@ if __name__ == '__main__':
 
 	# print(len(node_list))
 	node_degree = G.degree(node_list)
+	#for g in nx.connected_component_subgraphs(G):
+		#print(nx.average_shortest_path_length(g))
+		#avg_path_list.append(nx.average_shortest_path_length(g))
+	#avg_path = max(avg_path_list)
+
 	sorted_x = sorted(node_degree.items(), key=operator.itemgetter(1) , reverse = True)
-	print((sorted_x[0:30]))
-	for i in range(30):
-		ans_list.append(sorted_x[i][0])
-	writefile(ans_list,'ans_v3.txt')
+	ans_list.append(sorted_x[0][0])
+	for num in range(len(node_list)):
+		
+		try:
+			s=nx.shortest_path_length(G, source=sorted_x[num][0], target=sorted_x[num+1][0])
+			print(sorted_x[num][0],sorted_x[num+1][0],s,G.degree(sorted_x[num][0]))
+		except nx.NetworkXNoPath:
+			s = -1
+			print(sorted_x[num][0],sorted_x[num+1][0],'No path')
+		
+		if( s > 5 ):
+			ans_list.append(sorted_x[num+1][0])
+		if(len(ans_list)==30):
+			break
+	#print((ans_list))
+	writefile(ans_list,'ans_v7.txt')
